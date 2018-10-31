b9edc9efd3ee336bd7242150fc1f4eb2015c7bb5
walz0 <aidan.m.walz@gmail.com>
2018-10-31 17:04:09 -0400

    Initial Commit

---
+++ grapher.js
@@ -0,0 +1,149 @@
+//Global Variables
+var axes = {}; //X and Y axes
+var time = 0; //Time delta
+var scope = { //Scope of variables within functions
+  x: 0,
+  t: 0
+}
+var tree;
+var expr; //Default Expression
+var color = "rgb(139, 233, 253)"; //Default color
+var levelOfDetail = 7;
+
+var canvas = document.getElementById("canvas");
+
+//Calling the timedInterval function 60 frames per second
+var interval = setInterval(timedInterval, 16.7);
+function timedInterval () {
+  time += 0.05;
+  draw();
+}
+
+//Update the color of the curve
+function updateColor() {
+  color = document.getElementById("colorPicker").value;
+}
+
+//Set an expression
+function setExpr(newExpr){
+  expr = newExpr;
+}
+
+//Update the current expression
+function addFunction() {
+  setExpr(document.getElementById("input").value);
+}
+
+//Update the current scale of the viewport
+function updateScale(increment) {
+  var ctx = document.getElementById("canvas").getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //var viewportScale = document.getElementById("scale").value;
+  axes.scale += increment;
+  draw();
+}
+
+function draw() {
+  var canvas = document.getElementById("canvas");
+  canvas.width = screen.width;
+  canvas.height = screen.height * .819;
+  if (null == canvas || !canvas.getContext) return;
+
+  var ctx = canvas.getContext("2d");
+  axes.scale = document.getElementById("scale").value;
+  axes.x0 = 0.5 + .5 * canvas.width;  // x0 pixels from left to x=0
+  axes.y0 = 0.5 + .5 * canvas.height; // y0 pixels from top to y=0
+
+  showAxes(ctx, axes);
+  plot(ctx, axes, expr, color, 2);
+
+  var derivative = true;
+  
+  if(derivative)
+  {
+    var xValue = math.eval(document.getElementById("xText").value);
+    var yValue = evaluateMathExpr(expr, document.getElementById("xText").value);
+    document.getElementById("yText").value = yValue;
+    
+    var dydx = math.derivative(expr, "x").toString();
+    document.getElementById("derivative").value = dydx;
+
+    var m = evaluateMathExpr(dydx, xValue);
+    document.getElementById("m").value = m;
+    var pointSlope = (-xValue * m) + yValue;
+    if(pointSlope < 0){
+      var tangentLine = m + "x" + pointSlope;
+    }
+    else
+    {
+      var tangentLine = m + "x" + "+" + pointSlope;
+    }
+    
+    document.getElementById("tanLine").value = tangentLine;
+    
+    plot(ctx, axes, tangentLine, "rgb(241, 121, 198)", 2); //Tangent line to curve at point x
+    //plot(ctx, axes, math.derivative(expr, "x").toString(), "rgb(241, 121, 198)", 2); //General Derivative Function
+  }
+}
+
+function plot (ctx, axes, expr, color, thickness) {
+  var xx, //The current 'x' value that is being plotted
+  yy, //The current 'y' value that is being plotted
+  dx = levelOfDetail, //The distance 'x' between each point that is plotted
+  x0 = axes.x0, //The 'x' value of the origin
+  y0 = axes.y0, //The 'y' value of the origin
+  scale = axes.scale; //The scale of the viewport
+
+  //The range of x values of which to loop through
+  var iMax = Math.round((ctx.canvas.width - x0) / dx);
+  var iMin = Math.round(-x0 / dx);
+
+  ctx.beginPath();
+  ctx.lineWidth = thickness;
+  ctx.strokeStyle = color;
+
+  //Plotting and evaluating the function at each point x
+  for (var i = iMin; i <= iMax; i++) {
+    xx = dx * i;
+    yy = scale * evaluateMathExpr(expr, xx / scale);
+
+    if (i == iMin) {
+      ctx.moveTo(x0 + xx, y0 - yy);
+    }
+    else {
+      ctx.lineTo(x0 + xx, y0 - yy);
+    }
+  }
+  ctx.stroke();
+}
+
+function evaluateMathExpr(expr, mathX) {
+  // Set values on the scope visible inside the math expression.
+  scope.x = mathX;
+  scope.t = time;
+
+  // Evaluate the previously parsed math expression with the
+  // new values for 'x' and 't' and return it.
+  return math.eval(expr, scope);
+}
+
+function showAxes(ctx, axes) {
+  var x0 = axes.x0
+  var w = ctx.canvas.width;
+
+  var y0 = axes.y0
+  var h = ctx.canvas.height;
+
+  var xmin = 0;
+  
+  ctx.beginPath();
+  ctx.strokeStyle = "rgb(128, 128, 128)";
+
+  ctx.moveTo(xmin, y0);
+  ctx.lineTo(w, y0);  // X axis
+
+  ctx.moveTo(x0, 0);
+  ctx.lineTo(x0, h);  // Y axis
+
+  ctx.stroke();
+}
