2fa84b2024837b3d29a4cde2105ef24af54f774b
walz0 <aidan.m.walz@gmail.com>
2018-11-03 12:02:35 -0400

    Initial Commit

--- grapher.js
+++ grapher.js
@@ -7,7 +7,7 @@ var scope = { //Scope of variables within functions
 var tree;
 var expr; //Default Expression
 var color = "rgb(139, 233, 253)"; //Default color
-var levelOfDetail = 7;
+var levelOfDetail = 13;
 var drawTickMarks = true;
 var time = 0; //Time delta
 
@@ -29,11 +29,12 @@ function startPan() {
 }
 
 function mouseDelta(event) {
+  canvas = document.getElementById("canvas");
   mousePos.x = event.clientX;
   mousePos.y = event.clientY;
   if(panning) {
-    originOffset.x = mousePos.x - 960;
-    originOffset.y = mousePos.y - 490;
+    originOffset.x = mousePos.x - (canvas.width / 2);
+    originOffset.y = mousePos.y - (canvas.height / 2) - 50;
   }
 }
 
@@ -83,7 +84,7 @@ function draw() {
   axes.x0 = originOffset.x + .5 * canvas.width;  // x0 pixels from left to x=0
   axes.y0 = originOffset.y + .5 * canvas.height; // y0 pixels from top to y=0
 
-  document.getElementById("xText").value = document.getElementById("xSlider").value;
+  //document.getElementById("xText").value = document.getElementById("xSlider").value;
 
   showAxes(ctx, axes);
   plot(ctx, axes, expr, color, 2);
@@ -112,40 +113,123 @@ function draw() {
     
     document.getElementById("tanLine").value = tangentLine;
     
-    plot(ctx, axes, tangentLine, "rgb(241, 121, 198)", 2); //Tangent line to curve at point x
-    //plot(ctx, axes, math.derivative(expr, "x").toString(), "rgb(241, 121, 198)", 2); //General Derivative Function
+    plot(ctx, axes, dydx, "rgb(241, 121, 198)", 2); //Tangent line to curve at point x
+    plot(ctx, axes, tangentLine, "#FF8000", 2);
+    //plotCurve(ctx, axes, math.derivative(expr, "x").toString(), "rgb(241, 121, 198)", 2); //General Derivative Function
   }
 }
 
-function plot (ctx, axes, expr, color, thickness) {
-  var xx, //The current 'x' value that is being plotted
-  yy, //The current 'y' value that is being plotted
-  dx = levelOfDetail, //The distance 'x' between each point that is plotted
-  x0 = axes.x0, //The 'x' value of the origin
-  y0 = axes.y0, //The 'y' value of the origin
-  scale = axes.scale; //The scale of the viewport
+function pointSlope(m, x, y) {
+  var result = (-x * m) + y;
+  return result;
+}
 
-  //The range of x values of which to loop through
-  var iMax = Math.round((ctx.canvas.width - x0) / dx);
-  var iMin = Math.round(-x0 / dx);
+function pyth(x, y) {
+  var result = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+  return result;
+}
 
+function plotVector(ctx, axes, expr, color, thickness)
+{
+  var vector = {
+    x: 0,
+    y: 0
+  };
+
+  //Parsing Input and Evaluting Components
+  vector.x = math.eval(
+    expr.substring(expr.indexOf("[") + 1, expr.indexOf(","))
+  );
+  vector.y = math.eval(
+    expr.substring(expr.indexOf(",") + 1, expr.indexOf("]"))
+  );
+  
   ctx.beginPath();
   ctx.lineWidth = thickness;
   ctx.strokeStyle = color;
 
-  //Plotting and evaluating the function at each point x
-  for (var i = iMin; i <= iMax; i++) {
-    xx = dx * i;
-    yy = scale * evaluateMathExpr(expr, xx / scale);
+  //Plotting the Vector
+  ctx.moveTo(axes.x0, axes.y0); //Move to starting point
 
-    if (i == iMin) {
-      ctx.moveTo(x0 + xx, y0 - yy);
-    }
-    else {
-      ctx.lineTo(x0 + xx, y0 - yy);
-    }
-  }
+  //Drawing line to head of Vector
+  ctx.lineTo(
+    axes.x0 + vector.x * axes.scale, 
+    axes.y0 + vector.y * -axes.scale
+  ); 
+
+  //Creating a 'slope' for the Vector
+  var m = vector.y / vector.x; 
+  //Moving to a point 0.2 units down the hypotenuse
+  var mPerp = -1 * vector.x / vector.y;
+  var a = 0.1/2; //Wingspan
+  var c = 0.131/2; //Length of Wing
+  var b = 0.15/2; //Distance from base of Arrow triangle to tip
+  var h = pyth(vector.x, vector.y) - b; //Magnitude - b
+  var g = pyth(a, h);
+  //RADIANS
+  var thetaI = Math.atan(a/h);
+  var thetaV = Math.atan(vector.y / vector.x);
+  var thetaE = thetaV - thetaI;
+  var baseX = g * Math.cos(thetaE);
+  var baseY = g * Math.sin(thetaE);
+  
+  //Drawing Arrow head
+  /*ctx.moveTo(
+    axes.x0 + (headBaseX - offset) * axes.scale, 
+    axes.y0 + ((mPerp * (headBaseX - offset)) + pointSlope(mPerp, headBaseX, headBaseY)) * -axes.scale
+  ); //Left
+  ctx.lineTo(
+    axes.x0 + vector.x * axes.scale, 
+    axes.y0 + vector.y * -axes.scale
+  ); */
+
+  ctx.moveTo(
+    axes.x0 + baseX * axes.scale, 
+    axes.y0 + baseY * -axes.scale
+  ); //Right
+  ctx.lineTo(
+    axes.x0 + vector.x * axes.scale, 
+    axes.y0 + vector.y * -axes.scale
+  ); 
+
   ctx.stroke();
+}
+
+function plot (ctx, axes, expr, color, thickness) {
+  //Vector
+  if(expr.includes("[")) {
+    plotVector(ctx, axes, expr, color, 2);
+  }
+  else { //Function
+    var xx, //The current 'x' value that is being plotted
+    yy, //The current 'y' value that is being plotted
+    dx = levelOfDetail, //The distance 'x' between each point that is plotted
+    x0 = axes.x0, //The 'x' value of the origin
+    y0 = axes.y0, //The 'y' value of the origin
+    scale = axes.scale; //The scale of the viewport
+  
+    //The range of x values of which to loop through
+    var iMax = Math.round((ctx.canvas.width - x0) / dx);
+    var iMin = Math.round(-x0 / dx);
+  
+    ctx.beginPath();
+    ctx.lineWidth = thickness;
+    ctx.strokeStyle = color;
+  
+    //Plotting and evaluating the function at each point x
+    for (var i = iMin; i <= iMax; i++) {
+      xx = dx * i;
+      yy = scale * evaluateMathExpr(expr, xx / scale);
+  
+      if (i == iMin) {
+        ctx.moveTo(x0 + xx, y0 - yy);
+      }
+      else {
+        ctx.lineTo(x0 + xx, y0 - yy);
+      }
+    }
+    ctx.stroke();
+  }
 }
 
 function evaluateMathExpr(expr, mathX) {
