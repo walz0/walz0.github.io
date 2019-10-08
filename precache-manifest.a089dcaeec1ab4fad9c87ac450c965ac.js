c50d33c4054abf0fa041849cb03876d28db79709
Aidan Walz <aidan.m.walz@gmail.com>
2019-10-08 19:03:05 -0400

    sdfsdf

---
+++ precache-manifest.a089dcaeec1ab4fad9c87ac450c965ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b7941cb0fba2b5cc7a628e5c33fa4a",
+    "url": "//walz0.github.io/palace/index.html"
+  },
+  {
+    "revision": "639d95358e34f1aa5ffd",
+    "url": "//walz0.github.io/palace/static/css/main.61cca2fc.chunk.css"
+  },
+  {
+    "revision": "95878338717786b97b93",
+    "url": "//walz0.github.io/palace/static/js/2.b159d0cc.chunk.js"
+  },
+  {
+    "revision": "639d95358e34f1aa5ffd",
+    "url": "//walz0.github.io/palace/static/js/main.d27b866c.chunk.js"
+  },
+  {
+    "revision": "eccfb2ccff30914e905f",
+    "url": "//walz0.github.io/palace/static/js/runtime-main.d130bc49.js"
+  }
+]);
